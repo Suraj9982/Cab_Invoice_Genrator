680899e506da87f685f18f1a8a1799c075255cd0
suraj9982 <surajsk9982@gmail.com>
2021-12-10 20:15:00 +0530

    [suraj] add : This Calculate multipleRidefair and Test The method

--- CabInvoiceGenerator/CabInvoiceGenerator/InvoiceGenerator.cs
+++ CabInvoiceGenerator/CabInvoiceGenerator/InvoiceGenerator.cs
@@ -6,19 +6,12 @@ using System.Threading.Tasks;
 
 namespace CabInvoiceGenerator
 {
-    public class InviceGenerator
+    public class InvoiceGenerator
     {
         readonly private double MIN_FARE = 5.0;
-        readonly private double DISTANCE;
-        readonly private  double TIME;
         readonly private double COST_PER_KM = 10.0;
         readonly private double COST_PER_MINUTE = 1.0;
-        public InviceGenerator(double RunningDistane,double RunningTime)
-        {
-            this.DISTANCE = RunningDistane;
-            this.TIME = RunningTime;
-        }
-        public double CalculateFare()
+        public double CalculateFare(double DISTANCE,double TIME)
         {
             double TotalFare = (DISTANCE * COST_PER_KM) + (TIME * COST_PER_MINUTE);
             if (TotalFare < MIN_FARE)
@@ -30,7 +23,21 @@ namespace CabInvoiceGenerator
                 return TotalFare;
             }
         }
-
+        public double CalculateMultipleRideFair(Ride[] ride)
+        {
+            double totalFare=0;
+            foreach(var data in ride)
+            {
+                totalFare += (data.DISTANCE * COST_PER_KM) + (data.TIME * COST_PER_MINUTE);
+            }
+            if (totalFare < MIN_FARE)
+            {
+                return MIN_FARE;
+            }
+            else
+            {
+                return totalFare;
+            }
+        }
     }
-
 }
--- CabInvoiceGenerator/CabInvoiceGeneratorTest/CabService.cs
+++ CabInvoiceGenerator/CabInvoiceGeneratorTest/CabService.cs
@@ -8,10 +8,19 @@ namespace CabInvoiceGeneratorTest
         [Test]
         public void GivenDistanceAndTime_WhenACalculate_shoouldReturntotalFare()
         {
-            InviceGenerator invoice = new InviceGenerator(5, 2);
-            double result = invoice.CalculateFare();
-            Assert.AreEqual(result, 52);
-
+            InvoiceGenerator invoice = new InvoiceGenerator();
+            double Expected = 52;
+            double result = invoice.CalculateFare(5,2);
+            Assert.AreEqual(Expected,result);
         }
-}
+        [Test]
+        public void GivenDistanceAndTime_MultipleRide_WhenACalculate_shoouldReturntotalFare()
+        {
+            InvoiceGenerator invoice = new InvoiceGenerator();
+            Ride[] rides = { new Ride(5, 2), new Ride(7, 2) };
+            double expected = invoice.CalculateMultipleRideFair(rides);
+            double result = invoice.CalculateMultipleRideFair(rides);
+            Assert.AreEqual(result, expected);
+        }
+    }
 }
